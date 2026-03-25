febe02ad8b00cb5bacc87fd0eef168ca980afd19
Noemie Corneillier <noemiecorneillier@gmail.com>
2026-03-25 14:26:44 -0400

    - Fixed bugs in AdminDashboard: TS2339: Property 'env' does not exist on type 'ImportMeta', TS2322: onClick does not exist on Card props - Created path for CreateSurvey.tsx page

--- frontend/src/components/common/Card.tsx
+++ frontend/src/components/common/Card.tsx
@@ -9,3 +9,25 @@ const Card = ({ children, className }: { children: React.ReactNode; className?: 
 }
 
 export default Card;
+
+// import { twMerge } from "tailwind-merge";
+// import type { HTMLAttributes, ReactNode } from "react";
+//
+// type CardProps = HTMLAttributes<HTMLDivElement> & {
+//   children: ReactNode;
+// };
+//
+// const Card = ({ children, className, ...props }: CardProps) => {
+//   return (
+//       <div
+//           {...props}
+//           className={twMerge(
+//               `w-full max-w-md rounded-lg bg-white p-6 shadow-md ${className || ""}`,
+//           )}
+//       >
+//         {children}
+//       </div>
+//   );
+// };
+//
+// export default Card;
---
+++ frontend/src/pages/CreateSurvey.tsx
@@ -0,0 +1,11 @@
+// http://localhost:5173/admin-dashboard/surveys/new
+
+export default function CreateSurvey() {
+
+
+    return (
+        <main>
+            <h1>Landed on create a survey page</h1>
+        </main>
+    );
+}
